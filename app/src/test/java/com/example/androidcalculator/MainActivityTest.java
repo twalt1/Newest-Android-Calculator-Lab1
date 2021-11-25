177c2624d64048be2a2421f78143b9dfde17f88d
JohnnyCanuck328 <91069350+JohnnyCanuck328@users.noreply.github.com>
2021-11-25 13:48:40 -0500

    subtraction testing updated

--- app/src/test/java/com/example/androidcalculator/MainActivityTest.java
+++ app/src/test/java/com/example/androidcalculator/MainActivityTest.java
@@ -22,21 +22,19 @@ public class MainActivityTest {
      */
     @Test
     public void test_Subtraction() {
-        double val1 = 7.0;
-        double val2 = 5.6;
-        double tolerance = 0.0000000001;
+        double tolerance = 0.001;
 
         MainActivity main = new MainActivity();
 
-        double test1 = main.mathEquals(7.0, 5.6, '-');
-        double test2 = main.mathEquals(-7.0, 5.6, '-');
-        double test3 = main.mathEquals(7.0, -5.6, '-');
-        double test4 = main.mathEquals(-7.0, -5.6, '-');
-        double test5 = main.mathEquals(5.6, 7.0, '-');
-        double test6 = main.mathEquals(100000, 468431, '-');
-        double test7 = main.mathEquals(616151, 4641, '-');
-        double test8 = main.mathEquals(1566.516156, 869.234, '-');
-        double test9 = main.mathEquals(4861.328, 894.3547, '-');
+        double test1 = Double.parseDouble(main.toSub(7.0, 5.6));
+        double test2 = Double.parseDouble(main.toSub(-7.0, 5.6));
+        double test3 = Double.parseDouble(main.toSub(7.0, -5.6));
+        double test4 = Double.parseDouble(main.toSub(-7.0, -5.6));
+        double test5 = Double.parseDouble(main.toSub(5.6, 7.0));
+        double test6 = Double.parseDouble(main.toSub(100000, 468431));
+        double test7 = Double.parseDouble(main.toSub(616151, 4641));
+        double test8 = Double.parseDouble(main.toSub(1566.516156, 869.234));
+        double test9 = Double.parseDouble(main.toSub(4861.328, 894.3547));
 
         assertEquals(1.4, test1, tolerance);
         assertEquals(-12.6, test2, tolerance);
